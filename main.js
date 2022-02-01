0ee025f3b8760190e9b229e5f12aa323ecf4ff27
unknown <hamzadujaily345@gmail.com>
2022-02-01 07:35:04 -0800

    add

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 
 
-document.querySelector('.filter-by-region .filter-head').onclick=function () {
-    document.querySelector('.filter-by-region ').classList.toggle('filter-height')
+
+document.querySelector('nav .switch-theme').onclick=function () {
+    this.classList.toggle('bColor')
+    this.children[0].classList.toggle('hidden')
+    this.children[1].classList.toggle('hidden')
+    document.querySelector('body').classList.toggle('dark')
+    document.querySelector('nav').classList.toggle('light-dark')
+    document.querySelector('.filter-by-region ul').classList.toggle('light-dark')
+    document.querySelector('.filter-head').classList.toggle('light-dark')
+    document.querySelectorAll('.box ').forEach((b)=>{
+        b.classList.toggle('box-dark')
+    })
 }
 //let countries=document.querySelector('.countries')
 let mainPageC=document.querySelector('.main-page .content')
@@ -45,19 +55,23 @@ function boxContent(i,data) {
     <div>
         <div class="container">
             <div><img src="${data[i].flags.png}"></div>
-            <div>
-                <h2>${data[i].name}</h2>
-                <h4>Population: <span>${data[i].population}</span></h4>
-                <h4>Sub Region: <span>${data[i].subregion}</span></h4>
-                <h4>Top Level Domain: <span>${data[i].region}</span></h4>
-                <h4>Languages: <span>${data[i].languages[0].name}</span></h4>
-                <h4>Border Countries: <span>${data[i].borders}</span></h4>
-            </div>
-            <div>
-                <h4>native Name: <span>${data[i].nativeName}</span></h4>
-                <h4>Region: <span>${data[i].region}</span></h4>
-                <h4>Capital: <span>${data[i].capital}</span></h4>
-                <h4>Currencies: <span>${data[i].currencies[0].name}</span></h4>
+            <div class="detail-content">
+                <h3>${data[i].name}</h3>
+                <div>
+                    <div>
+                        <p>Population: <span>${data[i].population}</span></p>
+                        <p>Sub Region: <span>${data[i].subregion}</span></p>
+                        <p>Top Level Domain: <span>${data[i].region}</span></p>
+                        <p>Languages: <span>${data[i].languages[0].name}</span></p>
+                    </div>
+                    <div>
+                        <p>native Name: <span>${data[i].nativeName}</span></p>
+                        <p>Region: <span>${data[i].region}</span></p>
+                        <p>Capital: <span>${data[i].capital}</span></p>
+                        <p>Currencies: <span>${data[i].currencies[0].name}</span></p>
+                    </div>
+                </div> 
+                <h3>Border Countries: <span>${data[i].borders}</span></h3>
             </div>
         </div>
         <button type="submit" class="back" onclick=backToCountries()>Back</button>
@@ -100,6 +114,10 @@ function searchValue(v){
         addCountries(vData)
     }
 }
+// region filter
+document.querySelector('.filter-by-region .filter-head').onclick=function () {
+    document.querySelector('.filter-by-region ').classList.toggle('filter-height')
+}
 // filter
 document.querySelectorAll('.filter-by-region .continent li').forEach((f)=>{
     f.addEventListener('click',()=>{
@@ -125,7 +143,7 @@ document.querySelectorAll('.filter-by-region .continent li').forEach((f)=>{
         }else{
             addCountries(vData)
         }
-        document.querySelector('.filter-by-region .filter-head').textContent=`Region:${f.textContent}`
+        document.querySelector('.filter-by-region .filter-head span').textContent=f.textContent
     })
 })
 
